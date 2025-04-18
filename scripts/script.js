2968c6f56800abe683947f73dfab8701e550a62b
Emmanuel Malik <emmanuel00440@gmail.com>
2025-04-18 10:49:20 +0100

    plus and minus buttons made interactive

---
+++ scripts/script.js
@@ -0,0 +1,62 @@
+/* FIRST SECTION */
+
+const plusElementOne = document.querySelector('.js-plus-icon-one');
+const minusElementOne = document.querySelector('.js-minus-icon-one');
+
+function plusClickedOne() {
+  plusElementOne.classList.add('remove-plus-one');
+  minusElementOne.classList.add('display-minus-one');
+}
+
+function minusClickedOne() {
+  plusElementOne.classList.remove('remove-plus-one');
+  minusElementOne.classList.remove('display-minus-one');
+}
+
+
+/* SECOND SECTION */
+
+const plusElementTwo = document.querySelector('.js-plus-icon-two');
+const minusElementTwo = document.querySelector('.js-minus-icon-two');
+
+function plusClickedTwo() {
+  plusElementTwo.classList.add('remove-plus-two');
+  minusElementTwo.classList.add('display-minus-two');
+}
+
+function minusClickedTwo() {
+  plusElementTwo.classList.remove('remove-plus-two');
+  minusElementTwo.classList.remove('display-minus-two');
+}
+
+
+/* THIRD SECTION */
+
+const plusElementThree = document.querySelector('.js-plus-icon-three');
+const minusElementThree = document.querySelector('.js-minus-icon-three');
+
+function plusClickedThree() {
+  plusElementThree.classList.add('remove-plus-three');
+  minusElementThree.classList.add('display-minus-three');
+}
+
+function minusClickedThree() {
+  plusElementThree.classList.remove('remove-plus-three');
+  minusElementThree.classList.remove('display-minus-three');
+}
+
+
+/* FOURTH SECTION */
+
+const plusElementFour = document.querySelector('.js-plus-icon-four');
+const minusElementFour = document.querySelector('.js-minus-icon-four');
+
+function plusClickedFour() {
+  plusElementFour.classList.add('remove-plus-four');
+  minusElementFour.classList.add('display-minus-four');
+}
+
+function minusClickedFour() {
+  plusElementFour.classList.remove('remove-plus-four');
+  minusElementFour.classList.remove('display-minus-four');
+}
